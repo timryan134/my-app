afcacb39aed19553406886d01cb791cdeb1292db
Timothy Ryan <tim.ryan134@gmail.com>
2021-01-17 06:32:24 -0600

    adjusted App.js to match code on udemy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+      return (
+      <div className="App">
+          <h1>Hi, I'm a React App</h1>
+      </div>
+      )
+  }
 }
 
 export default App;
